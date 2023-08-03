b0c08d254ec6bee2019f035af7b103552d5a2eba
Jeongwoo Lee <juny1alex@gmail.com>
2023-08-03 16:01:57 +0900

    mid-presentation

--- frontend/views/main_views.py
+++ frontend/views/main_views.py
@@ -12,30 +12,6 @@ sys.path.append('../../')
 from src import embeddings
 bp = Blueprint('main',__name__,url_prefix='/')
 
-# def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
-#     try:
-#         n = np.fromfile(filename, dtype)
-#         img = cv2.imdecode(n, flags)
-#         return img
-#     except Exception as e:
-#         print(e)
-#         return None
-#
-# def imwrite(filename, img, params=None):
-#     try:
-#         ext = os.path.splitext(filename)[1]
-#         result, n = cv2.imencode(ext, img, params)
-#
-#         if result:
-#             with open(filename, mode='w+b') as f:
-#                 n.tofile(f)
-#             return True
-#         else:
-#             return False
-#     except Exception as e:
-#         print(e)
-#         return False
-
 @bp.route('/')
 def main():
     return render_template('main/main.html',client_img=False,faculty=False)
@@ -47,6 +23,7 @@ def create():
     path = "./frontend/static/client_img/"+(file.filename)
     path_ = "./static/client_img/"+(file.filename)
     file.save(path)
+    keras.backend.clear_session()
     model = keras.models.load_model('./model/facenet_keras.h5')
     emb = embeddings.get_embedding_from_one_pic(model, path)
     faculty_json = open('./model/faculty_emb.json',encoding='utf-8')
@@ -62,4 +39,5 @@ def create():
     department = name.split('_')[0]
     name_ = name.split('_')[1]
     faculty_path = "./static/faculty_img/"+name+".jpg"
+
     return render_template('main/main.html', client_img=path_, faculty=faculty_path, department=department, name=name_)
